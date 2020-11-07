1f85c851ddd4faf8046bd33f97ac84c507f10b23
muhammadhasyim <muhammad_hasyim@berkeley.edu>
2020-11-06 22:38:58 -0800

    Updated setup/CMake scripts to copy pybind11 + moved pybind11 dependenxy from FTSSampler.h to module-fts.cpp

--- tpstorch/fts/FTSSampler.h
+++ tpstorch/fts/FTSSampler.h
@@ -3,10 +3,6 @@
 #define __FTS_SAMPLER_H__
 
 #include <torch/torch.h>
-#include <torch/extension.h>
-#include <pybind11/pybind11.h>
-
-namespace py = pybind11;
 
 class FTSSampler
 {
@@ -31,14 +27,5 @@ class FTSSampler
     private:
 };
 
-void export_FTSSampler(py::module& m)
-{
-    py::class_<FTSSampler, std::shared_ptr<FTSSampler> > (m, "FTSSampler")
-    .def(py::init<>())
-    .def("runSimulation", &FTSSampler::runSimulation)
-    .def("getConfig", &FTSSampler::getConfig)
-    .def("dumpConfig", &FTSSampler::dumpConfig)
-    ;
-};
 
 #endif //__FTS_SAMPLER_H__
--- tpstorch/fts/module-fts.cpp
+++ tpstorch/fts/module-fts.cpp
@@ -1,5 +1,18 @@
 #include "FTSSampler.h"
 #include <torch/extension.h>
+#include <pybind11/pybind11.h>
+
+namespace py = pybind11;
+
+void export_FTSSampler(py::module& m)
+{
+    py::class_<FTSSampler, std::shared_ptr<FTSSampler> > (m, "FTSSampler")
+    .def(py::init<>())
+    .def("runSimulation", &FTSSampler::runSimulation)
+    .def("getConfig", &FTSSampler::getConfig)
+    .def("dumpConfig", &FTSSampler::dumpConfig)
+    ;
+};
 
 PYBIND11_MODULE(_fts, m)
 {
