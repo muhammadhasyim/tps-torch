d534413c42ac972b83eee9ede26a55024bfdc219
muhammadhasyim <muhammad_hasyim@berkeley.edu>
2020-11-13 10:17:36 -0800

    Moved state initialization to constructor

--- muller-brown/mysampler.cpp
+++ muller-brown/mysampler.cpp
@@ -5,7 +5,7 @@ namespace py = pybind11;
 void export_MySampler(py::module& m)
 {
     py::class_<MySampler, FTSSampler, std::shared_ptr<MySampler> > (m, "MySampler")
-    .def(py::init< std::string >())
+    .def(py::init< std::string, torch::Tensor >())
     .def("runSimulation", &MySampler::runSimulation)
     .def("getConfig", &MySampler::getConfig)
     .def("dumpConfig", &MySampler::dumpConfig)
