111b353b8b7d326af0c3a457dcaad382a14fa2f2
Clay Batton <cbatton35@gmail.com>
2020-11-29 22:34:44 -0800

    Added testing version with Voronoi stuff. Now onto actually seeing it works! Program builds but that doesn't give me a bunch of confidence with the amount of surgery I have done without testing

--- tpstorch/fts/__init__.py
+++ tpstorch/fts/__init__.py
@@ -288,3 +288,142 @@ class AltFTSMethod:
                         io.write("{} ".format(self.string[counter+1,i,j]))
                 io.write("\n")
         self.sampler.dumpConfig()
+
+#FTSMethod but matches that used in 2009 paper
+# A few things I liked to try here as well that I'll try to get with options, but bare for now
+class FTSMethodVor:
+    def __init__(self, sampler, initial_config, final_config, num_nodes, deltatau, kappa):
+        #The MD Simulation object, which interfaces with an MD Library
+        self.sampler = sampler
+        #String timestep 
+        self.deltatau = deltatau
+        #Regularization strength
+        self.kappa = kappa*num_nodes*deltatau
+        #Number of nodes including endpoints
+        self.num_nodes = dist.get_world_size()
+        #Number of samples in the running average
+        self.nsamples = 0
+        #Timestep
+        self.timestep = 0
+        
+        #Saving the typical configuration size
+        #TO DO: assert the config_size as defining a rank-2 tensor. Or else abort the simulation!
+        self.config_size = initial_config.size()
+        
+        #Store rank and world size
+        self.rank = dist.get_rank()
+        self.world = dist.get_world_size()
+
+        # Matrix used for inversing
+        # Construct only on rank 0
+        # Note that it always stays the same, so invert here and use at each iteration
+        self.matrix = 0
+        self.matrix_inverse = 0
+        if(self.rank == 0):
+            # Kinda confusing notation, but essentially to make this tridiagonal order is
+            # we go through each direction in order
+            # zeros
+            self.matrix = torch.zeros(self.config_size*self.num_nodes, self.config_size*self.num_nodes, dtype=torch.float)
+            # first, last row
+            for i in range(self.config_size):
+                self.matrix[i*self.num_nodes,i*self.num_nodes] = 1.0
+                self.matrix[(i+1)*self.num_nodes-1,(i+1)*self.num_nodes] = 1.0
+            # rest of rows
+            for i in range(self.config_size):
+                for j in range(1,self.num_nodes-1):
+                    self.matrix[i*self.num_nodes+j,i*self.num_nodes+j] = 1.0+2.0*self.kappa
+                    self.matrix[i*self.num_nodes+j,i*self.num_nodes+j-1] = -1.0*self.kappa
+                    self.matrix[i*self.num_nodes+j,i*self.num_nodes+j+1] = -1.0*self.kappa
+            # inverse
+            self.matrix_inverse = torch.inverse(self.matrix)
+        
+        #Nodal parameters
+        self.alpha = self.rank/(self.world-1)
+        
+        self.string = torch.lerp(initial_config,final_config,dist.get_rank()/(dist.get_world_size()-1))
+        self.avgconfig = torch.zeros_like(self.string)
+             
+        self.string_io = open("string_{}.xyz".format(dist.get_rank()+1),"w")
+        
+        #Initialize the Voronoi cell  
+        # Could maybe make more efficient by looking at only the closest nodes
+        self.voronoi = torch.empty(self.world, self.config_size, dtype=torch.float)
+    
+    def send_strings(self):
+        # Use an all-gather to communicate all strings to each other
+        dist.all_gather(self.voronoi, self.string)
+        
+    #Update the string. Since it only exists in the first rank, only the first rank gets to do this
+    def update(self):
+        ## (1) Regularized Gradient Descent
+        # Will use matrix solving in the near future, but for now use original update scheme
+        # Will probably make it an option to use explicit or implicit scheme
+        if self.rank > 0 and self.rank < self.world-1:
+            self.string += -self.deltatau*(self.string-self.avgconfig)+self.kappa*(self.voronoi[self.rank-1,:]-2*self.string+self.voronoi[self.rank+1,:])
+        elif self.rank == 0:
+            self.string -= self.deltatau*(self.string-self.avgconfig)
+        else:
+            self.string -= self.deltatau*(self.string-self.avgconfig)
+        
+        ## (2) Re-parameterization/Projection
+        ## Fist, Send the new intermediate string configurations
+        self.send_strings()
+        ## Next, compute the length segment of each string 
+        ell_k = torch.tensor(0.0)
+        if self.rank >= 0  and self.rank < self.world -1:
+            ell_k = torch.norm(self.voronoi[self.rank+1,:]-self.string)
+
+        ## Next, compute the arc-length parametrization of the intermediate configuration
+        list_of_ell = []
+        for i in range(self.world):
+            list_of_ell.append(torch.tensor(0.0))
+        dist.all_gather(tensor_list=list_of_ell, tensor=ell_k)
+        #REALLY REALLY IMPORTANT. this interpolation assumes that the intermediate configuration lies between the left and right neighbors 
+        #of the desired  configuration,
+        if self.rank > 0 and self.rank < self.world-1: 
+            del list_of_ell[-1]
+        
+            ellsum = sum(list_of_ell)
+            intm_alpha = torch.zeros(self.num_nodes)
+            for i in range(1,self.num_nodes):
+                intm_alpha[i] += list_of_ell[i-1].detach().clone()/ellsum+intm_alpha[i-1].detach().clone()
+            #Now interpolate back to the correct parametrization
+            index = torch.bucketize(self.alpha,intm_alpha)
+            weight = (self.alpha-intm_alpha[index-1])/(intm_alpha[index]-intm_alpha[index-1])
+            if index == self.rank+1:
+                self.string = torch.lerp(self.string,self.voronoi[self.rank+1,:],weight) 
+            elif index == self.rank:
+                self.string = torch.lerp(self.voronoi[self.rank-1,:],self.string,weight) 
+            else:
+                raise RuntimeError("You need to interpolate from points beyond your nearest neighbors. \n \
+                                    Reduce your timestep for the string update!")
+        #REALLY REALLY IMPORTANT. this interpolation assumes that the intermediate configuration lies between the left and right neighbors 
+        #of the desired  configuration,
+        #If not the case, set config equal to string center
+        #Not ideal, but will leave that to code implementation as there are a 
+        #bunch of tricky things I don't want to assume here (namely periodic
+        #boundary condition)
+        self.send_strings()
+
+    #Will make MD simulation run on each window
+    def run(self, n_steps):
+        #Do one step in MD simulation, constrained to Voronoi cells
+        self.sampler.runSimulation(n_steps,self.rank,self.voronoi)
+        
+        #Compute the running average
+        self.avgconfig = (self.sampler.getConfig()+self.nsamples*self.avgconfig).detach().clone()/(self.nsamples+1)
+        
+        #Update the string
+        self.update()
+        self.timestep += 1
+    #Dump the string into a file
+    def dump(self,dumpstring=False):
+        if dumpstring and self.rank == 0:
+            for counter, io in enumerate(self.string_io):
+                io.write("{} \n".format(self.config_size[0]))
+                io.write("# step {} \n".format(self.timestep))
+                for i in range(self.config_size[0]):
+                    for j in range(self.config_size[1]):
+                        io.write("{} ".format(self.string[counter+1,i,j]))
+                io.write("\n")
+        self.sampler.dumpConfig()
