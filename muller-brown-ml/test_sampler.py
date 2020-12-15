f0301d14d7b9719fbd44399b444612e8f6f18e30
Clay Batton <cbatton35@gmail.com>
2020-12-15 14:02:54 -0800

    Added basic script to test MPI stuff

--- muller-brown-ml/test_sampler.py
+++ muller-brown-ml/test_sampler.py
@@ -1,2 +1,8 @@
 import mullerbrown_ml as mb
-a = mb.MySampler("param",torch.tensor([[0.0,1.0]]),0,0,0,0)
+import torch
+import torch.distributed as dist
+
+dist.init_process_group(backend='mpi')
+mpi_group = dist.distributed_c10d._get_default_group()
+
+a = mb.MySampler('param',torch.tensor([[0.0,0.0]]),0,0,0.0,0.0,dist.distributed_c10d._get_default_group())
