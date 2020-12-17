5cc07f8fca9626c4baabd7c36a739454c6edd4fc
Clay Batton <cbatton35@gmail.com>
2020-12-17 03:15:58 -0800

    Trying the code with the harder Muller-Brown surface (hence tricky), and the thing does not want to work. Neural network always gives pack linear contours, sometimes goes crazy for a second along with larger fluctuations in the contours. Hmmm. Trying with more windows

--- muller-brown-ml/plot_tricky_lines.py
+++ muller-brown-ml/plot_tricky_lines.py
@@ -28,25 +28,18 @@ def energy(x,y,A,a,b,c,x_,y_):
         energy_ += A[i]*np.exp(a[i]*(x-x_[i])**2+b[i]*(x-x_[i])*(y-y_[i])+c[i]*(y-y_[i])**2)
     return energy_
 
-def q(xval,yval):
-    qvals = np.zeros_like(xval)
-    for i in range(nx):
-        for j in range(ny):
-            Array = np.array([xval[i,j],yval[i,j]]).astype(np.float32)
-            Array = torch.from_numpy(Array)
-            qvals[i,j] = committor(Array).item()
-    return qvals
 nx, ny = 100,100
 X = np.linspace(-2.0, 1.5, nx)
 Y = np.linspace(-1.0, 2.5, ny)
 print(X.shape)
 
+# now plot lines on top of it
+
 xv, yv = np.meshgrid(X, Y)
 z = energy(xv,yv,A,a,b,c,x_,y_)
 h = plt.contourf(X,Y,z,levels=[-15+i for i in range(16)])
+plt.plot(X,X+1)
+plt.plot(X,0.5*(X-1)+2)
 print(np.shape(z),np.shape(xv))
-qvals = q(xv,yv)
-CS = plt.contour(X, Y, qvals,levels=[0.01,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,0.99])
 plt.colorbar()
-plt.clabel(CS, fontsize=10, inline=1)#
 plt.show()
