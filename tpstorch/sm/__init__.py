057eb3ee59cab61d548d02d2105692d0a5fb6b63
muhammadhasyim <muhammad_hasyim@berkeley.edu>
2021-04-26 16:54:49 -0700

    Updated setup.py and removed sm module

--- tpstorch/sm/__init__.py
+++
@@ -1,6 +0,0 @@
-from . import _sm
-
-#Inherit the C++ compiled sampler class, and add the MPI process group by default
-class Sampler(_sm.Sampler):
-    def __init__(self):
-        super(Sampler,self).__init__()
