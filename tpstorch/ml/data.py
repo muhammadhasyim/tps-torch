edcb2e05e23123757d5e00557fd444cf4f5a4dbb
muhammadhasyim <muhammad_hasyim@berkeley.edu>
2021-05-10 12:21:01 -0700

    Update data.py so that we use the new simulation runner instead of the old one.

--- tpstorch/ml/data.py
+++ tpstorch/ml/data.py
@@ -3,7 +3,7 @@ from torch.utils.data import IterableDataset
 from itertools import cycle
 import tqdm
 import numpy as np
-
+"""
 class EXPReweightSimulation(IterableDataset):
     
     def __init__(self, sampler, committor, period):
@@ -66,9 +66,9 @@ class EXPReweightSimulation(IterableDataset):
     def __iter__(self):
         #Cycle through every period indefinitely
         return cycle(self.runSimulation())
+"""
 
-class EXPReweightSimulationManual:
-
+class EXPReweightSimulation:
     def __init__(self, sampler, committor, period, batch_size, dimN):
         ## Store the MD/MC Simulator, which samples our data
         self.sampler = sampler
@@ -98,16 +98,26 @@ class EXPReweightSimulationManual:
         
         #Backprop to compute gradients w.r.t. x
         self.out.backward()
-
     def runSimulation(self):
-        # Create storage entries
+        ## Create storage entries
+        
+        #Configurations for one mini-batch
         configs = torch.zeros(self.batch_size,self.dimN)
+        
+        #Gradient of committor w.r.t. x for every x in configs
         grads = torch.zeros(self.batch_size,self.dimN)
+        
+        #1/c(x) constant for every x in configs
         reciprocal_normconstant = torch.zeros(self.batch_size)
+        
+        #w_{l+1}/w_{l} computed for every x in configs
         fwd_weightfactor = torch.zeros(self.batch_size, 1)
+        
+        #w_{l-1}/w_{l} computed for every x in configs
         bwrd_weightfactor = torch.zeros(self.batch_size, 1)
         for i in range(self.batch_size):
             for j in range(self.period):
+                #print(j,self.sampler.torch_config.grad)
                 #Take one step
                 self.sampler.step(self.out,onlytst=False)
                 
@@ -116,6 +126,15 @@ class EXPReweightSimulationManual:
                 
                 #Zero out any gradients
                 self.committor.zero_grad()
+                
+                #Forward pass
+                self.out = self.committor(self.sampler.torch_config)
+                
+                #Compute the first set of reweighting factors from our initial condition
+                #self.sampler.computeFactors(self.out)
+                
+                #Backprop to compute gradients w.r.t. x
+                self.out.backward()
 
             if torch.sum(torch.isnan(self.out)) > 0:
                 raise ValueError("Committor value is NaN!")
@@ -128,12 +147,14 @@ class EXPReweightSimulationManual:
                 
                 #Backprop to compute gradients of x
                 self.out.backward()
-
+                
+                #Compute all for all storage entries
                 configs[i,:] = self.sampler.torch_config
                 grads[i,:] = torch.autograd.grad(self.committor(self.sampler.torch_config), self.sampler.torch_config, create_graph=True)[0]
                 reciprocal_normconstant[i] = self.sampler.reciprocal_normconstant
                 fwd_weightfactor[i,:] = self.sampler.fwd_weightfactor
                 bwrd_weightfactor[i,:] = self.sampler.bwrd_weightfactor
+        
         return configs, grads, reciprocal_normconstant, fwd_weightfactor, bwrd_weightfactor
 
 
