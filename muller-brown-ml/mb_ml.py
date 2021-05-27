92eafb9184992fa6868108b507a6394756b85b10
cbatton35 <cbatton35@gmail.com>
2021-05-27 04:52:05 -0700

    Adding code in the new format for standard method and committor loss for MB potential. There's a bug somewhere causing the weights to be very low, so it's not right. Will identify later

--- muller-brown-ml/mb_ml.py
+++ muller-brown-ml/mb_ml.py
@@ -5,69 +5,124 @@ from torch.utils.data import DataLoader
 import torch.nn as nn
 
 #Import necessarry tools from tpstorch 
-from tpstorch.ml.optim import project_simplex
 from tpstorch.ml import MLSamplerEXP
-from tpstorch.ml.nn import CommittorLossEXP
+from mullerbrown_ml import MySampler
 import numpy as np
 
 #Import any other thing
 import tqdm, sys
 
-dist.init_process_group(backend='mpi')
+#dist.init_process_group(backend='mpi')
 
 class CommittorNet(nn.Module):
-    def __init__(self, d, num_nodes, unit=torch.sigmoid):
+    def __init__(self, d, num_nodes, unit=torch.relu):
         super(CommittorNet, self).__init__()
         self.num_nodes = num_nodes
         self.d = d
         self.unit = unit
         self.lin1 = nn.Linear(d, num_nodes, bias=True)
+        self.lin3 = nn.Linear(num_nodes, num_nodes, bias=True)
         self.lin2 = nn.Linear(num_nodes, 1, bias=False)
-        self.project()
         self.broadcast()
 
     def forward(self, x):
         x = self.lin1(x)
         x = self.unit(x)
+        x = self.lin3(x)
+        x = self.unit(x)
         x = self.lin2(x)
-        return x
+        return torch.sigmoid(x)
     
     def broadcast(self):
         for name, param in self.named_parameters():
             if param.requires_grad:
                 dist.broadcast(param.data,src=0)
-    def project(self):
-        #Project the coefficients so that they are make the output go from zero to one
-        with torch.no_grad():
-            self.lin2.weight.data = project_simplex(self.lin2.weight.data)
 
-class MullerBrown(MLSamplerEXP):
-    #I'm wrapping the sampler in a sampler
-    #Lord praise have mercy on my soul
-    def __init__(self, sampler):
-        super(MullerBrown, self).__init(sampler)
-    
+class MullerBrown(MySampler):
+    def __init__(self,param,config,rank,dump,beta,kappa,mpi_group,committor,save_config=False):
+        super(MullerBrown, self).__init__(param,config.detach().clone(),rank,dump,beta,kappa,mpi_group)
+        #super(MullerBrown, self).__init__(config.detach().clone())
+        #super(MullerBrown, self).__init__(param,config.detach().clone(),rank,dump,beta,kappa,mpi_group)
+
+        self.committor = committor
+
+        self.save_config = save_config
+        self.timestep = 0
+
+        #Save config size and its flattened version
+        self.config_size = config.size()
+        self.flattened_size = config.flatten().size()
+        self.torch_config = config
+
+    @torch.no_grad() 
     def initialize_from_torchconfig(self, config):
+        # Don't have to worry about all that much all, can just set it
+        self.setConfig(config)
 
     def step(self, committor_val, onlytst=False):
+        with torch.no_grad():
+            config_test = torch.zeros_like(self.torch_config)
+            self.propose(config_test, committor_val, onlytst)
+            committor_val_ = self.committor(config_test)
+            self.acceptReject(config_test, committor_val_, onlytst, True)
+        self.torch_config = (self.getConfig().flatten()).detach().clone()
+        self.torch_config.requires_grad_()
+        try:
+            self.torch.grad.data.zero_()
+        except:
+            pass
 
     def step_unbiased(self):
+        with torch.no_grad():
+            config_test = torch.zeros_like(self.torch_config)
+            self.propose(config_test, 0.0, False)
+            self.acceptReject(config_test, 0.0, False, False)
+        self.torch_config = (self.getConfig().flatten()).detach().clone()
+        self.torch_config.requires_grad_()
+        try:
+            self.torch.grad.data.zero_()
+        except:
+            pass
 
-    def step_unbiased(self):
+    @torch.no_grad() 
+    def isProduct(self,config):
+        end = torch.tensor([[0.6,0.08]])
+        radii = 0.025
+        end_ = config-end
+        end_ = end_.pow(2).sum()**0.5
+        if end_ <= radii:
+            return True
+        else:
+            return False
 
-class MullerBrownLoss(CommittorLossEXP):
-    def __init__(self, lagrange_bc, batch_size):
-        super(MullerBrownLoss,self).__init__()
-        self.lagrange_bc = lagrange_bc
+    @torch.no_grad() 
+    def isReactant(self,config):
+        start = torch.tensor([[-0.5,1.5]])
+        radii = 0.025
+        start_ = config-start
+        start_ = start_.pow(2).sum()**0.5
+        if start_ <= radii:
+            return True
+        else:
+            return False
 
-    def compute_bc(self, committor, configs, invnormconstants):
-        #Assume that first dimension is batch dimension
-        loss_bc = torch.zeros(1)
-        for i, config in enumerate(configs):
-            print("CONFIGS PRINTING")
-            print(config)
-            if config.item() <= -1:
-                loss_bc += 0.5*self.lagrange_bc*(committor(config.flatten())**2)*invnormconstants[i]
-            if config.item() >= 1:
-                loss_bc += 0.5*self.lagrange_bc*(committor(config.flatten())-1.0)**2*invnormconstants[i]
-        return loss_bc/(i+1)
+    def step_bc(self):
+        with torch.no_grad():
+            config_test = torch.zeros_like(self.torch_config)
+            self.propose(config_test, 0.0, False)
+            if self.isReactant(config_test.flatten()) or self.isProduct(config_test.flatten()):
+                self.acceptReject(config_test, 0.0, False, False)
+            else:
+                pass
+        self.torch_config = (self.getConfig().flatten()).detach().clone()
+        self.torch_config.requires_grad_()
+        try:
+            self.torch.grad.data.zero_()
+        except:
+            pass
+
+    def save(self):
+        self.timestep += 1
+        if self.save_config:
+            if self.timestep % 100 == 0:
+                self.dumpConfig(0)
---
+++ muller-brown-ml/run_vanilla.py
@@ -0,0 +1,147 @@
+#import sys
+#sys.path.insert(0,"/global/home/users/muhammad_hasyim/tps-torch/build")
+
+#Import necessarry tools from torch
+import tpstorch
+import torch
+import torch.distributed as dist
+import torch.nn as nn
+
+#Import necessarry tools from tpstorch 
+from mb_ml import MullerBrown, CommittorNet
+from tpstorch.ml.data import EXPReweightSimulation
+from tpstorch.ml.optim import ParallelSGD, ParallelAdam
+from tpstorch.ml.nn import BKELossEXP
+import numpy as np
+
+#Grag the MPI group in tpstorch
+mpi_group = tpstorch._mpi_group
+world_size = tpstorch._world_size
+rank = tpstorch._rank
+
+#Import any other thing
+import tqdm, sys
+torch.manual_seed(3046)
+np.random.seed(3046)
+
+prefix = 'simple'
+
+#Initialize neural net
+def initializer(s):
+    return (1-s)*start+s*end
+start = torch.tensor([-0.5,1.5])
+end = torch.tensor([0.6,0.08])
+
+#Initialize neural net
+committor = CommittorNet(d=2,num_nodes=200).to('cpu')
+
+initial_config = initializer(rank/(dist.get_world_size()-1))
+start = torch.tensor([[-0.5,1.5]])
+end = torch.tensor([[0.6,0.08]])
+
+kT = 10.0
+mb_sim = MullerBrown(param="param",config=initial_config, rank=rank, dump=1, beta=1/kT, save_config=True, mpi_group = mpi_group, kappa=20000, committor=committor)
+mb_sim_bc = MullerBrown(param="param_bc",config=initial_config, rank=rank, dump=1, beta=1/kT, save_config=True, mpi_group = mpi_group, kappa=0, committor=committor)
+
+#Initial Training Loss
+initloss = nn.MSELoss()
+initoptimizer = ParallelSGD(committor.parameters(), lr=1e-3)#,momentum=0.95, nesterov=True)
+
+#from torchsummary import summary
+running_loss = 0.0
+#Initial training try to fit the committor to the initial condition
+for i in range(3*10**3):
+    # zero the parameter gradients
+    initoptimizer.zero_grad()
+    # forward + backward + optimize
+    q_vals = committor(initial_config.view(-1,2))
+    targets = torch.ones_like(q_vals)*rank/(dist.get_world_size()-1)
+    cost = initloss(q_vals, targets)#,committor,config,cx)
+    cost.backward()
+    #Stepping up
+    initoptimizer.step()
+    if i%1000 == 0 and rank == 0:
+        print("Init step "+str(i),cost)
+if rank == 0:
+    #Only save parameters from rank 0
+    torch.save(committor.state_dict(), "{}_params_{}".format(prefix,rank+1))
+committor.zero_grad()
+
+n_boundary_samples = 100
+batch_size = 128
+period = 10
+datarunner = EXPReweightSimulation(mb_sim, committor, period=period, batch_size=batch_size, dimN=2)
+
+#Initialize main loss function and optimizers
+loss = BKELossEXP(  bc_sampler = mb_sim_bc, 
+                    committor = committor,
+                    lambda_A = 1e4,
+                    lambda_B = 1e4,
+                    start_react = start, 
+                    start_prod = end, 
+                    n_bc_samples = 100, 
+                    bc_period = 10,
+                    batch_size_bc = 0.5)
+
+#This is the new committor loss! Feel free to comment this out if you don't need it
+#cmloss = FTSCommittorLoss(  fts_sampler = mb_sim,
+#                            committor = committor,
+#                            fts_layer=ftslayer, 
+#                            dimN = 2,
+#                            lambda_fts=1e-1,
+#                            fts_start=200,  
+#                            fts_end=2000,
+#                            fts_max_steps=batch_size*period*4, #To estimate the committor, we'll run foru times as fast 
+#                            fts_rate=4, #In turn, we will only sample more committor value estimate  after 4 iterations 
+#                            fts_min_count=2000, #Minimum count so that simulation doesn't (potentially) run too long
+#                            batch_size_fts=0.5,
+#                            tol = 1e-6,
+#                            mode = 'shift'
+#                            )
+
+#optimizer = ParallelAdam(committor.parameters(), lr=1e-2)
+optimizer = ParallelSGD(committor.parameters(), lr=1e-3)#,momentum=0.95,nesterov=True)
+
+loss_io = []
+if rank == 0:
+    loss_io = open("{}_loss.txt".format(prefix),'w')
+
+#Training loop
+#We can train in terms of epochs, but we will keep it in one epoch
+for epoch in range(1):
+    if rank == 0:
+        print("epoch: [{}]".format(epoch+1))
+    actual_counter = 0
+    while actual_counter <= 20000:
+        # get data and reweighting factors
+        configs, grad_xs, invc, fwd_wl, bwrd_wl  = datarunner.runSimulation()
+        
+        # zero the parameter gradients
+        optimizer.zero_grad()
+        
+        # (2) Update the neural network
+        cost = loss(grad_xs, invc, fwd_wl, bwrd_wl)
+        
+        totalcost = cost#+cmcost
+        totalcost.backward()
+        optimizer.step()
+        
+        # print statistics
+        with torch.no_grad():
+            #if counter % 10 == 0:
+            main_loss = loss.main_loss
+            bc_loss = loss.bc_loss
+            
+            #Print statistics 
+            if rank == 0:
+                print('[{}] main_loss: {:.5E} bc_loss: {:.5E} lr: {:.3E}'.format(actual_counter + 1, main_loss.item(), bc_loss.item(),  optimizer.param_groups[0]['lr']),flush=True)
+                print(loss.zl)
+                
+                loss_io.write('{:d} {:.5E} {:.5E} \n'.format(actual_counter+1,main_loss.item(),bc_loss.item()))
+                loss_io.flush()
+                
+                if actual_counter % 100 == 0:
+                    torch.save(committor.state_dict(), "{}_params_t_{}_{}".format(prefix,actual_counter,rank))
+                
+                torch.save(committor.state_dict(), "{}_params_{}".format(prefix,rank+1))
+        actual_counter += 1
