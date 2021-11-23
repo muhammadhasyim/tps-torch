2e7d36beb825bc0d3bf4eed4841dd4e384e9eb1e
Clay Batton <cbatton35@gmail.com>
2021-11-23 08:35:27 -0800

    Adding base of dimer code. Something is very wrong in there, but at least the interface is in place. Will debug more and get to match with another code

---
+++ tpstorch/examples/dimer_ml/dimer.cpp
@@ -0,0 +1,350 @@
+#include <iostream> 
+#include <fstream>
+#include <random>
+#include <algorithm>
+#include <math.h>
+#include <ctime>
+#include <string>
+#include <vector>
+#include <chrono>
+#include "saruprng.hpp"
+#include "dimer.h"
+using namespace std;
+
+Dimer::Dimer() {
+    // Constructor, current does nothing but prepare state
+    state.resize(2, vector<float>(3,0));
+}
+
+Dimer::~Dimer() {
+    // Destructor, no pointers so no worries
+}
+
+void Dimer::GetParams(string name, int rank_in) {
+    ifstream input;
+    input.open(name);
+    if(input.fail()) {
+        cout << "No input file" << endl;
+    }
+    else {
+        char buffer;
+        string line;
+        //cout << "Param file detected. Changing values." << endl;
+        input >> line >> temp;
+        //cout << "temp is now " << temp << endl;
+        getline(input, line);
+        input >> line >> mass;
+        //cout << "mass is now " << mass << endl;
+        getline(input, line);
+        input >> line >> gamma;
+        //cout << "gamma is now " << gamma << endl;
+        getline(input, line);
+        input >> line >> dt;
+        //cout << "dt is now " << dt << endl;
+        getline(input, line);
+        input >> line >> box[0] >> box[1] >> box[2];
+        //cout << "box is now " << box[0] << " " << box[1] << " " << box[2] << endl;
+        getline(input, line);
+        input >> line >> height;
+        //cout << "height is now " << height << endl;
+        getline(input, line);
+        input >> line >> r_0;
+        //cout << "r_0 is now " << r_0 << endl;
+        getline(input, line);
+        input >> line >> width;
+        //cout << "width is now " << width << endl;
+        getline(input, line);
+        input >> line >> dist_init;
+        //cout << "dist_init is now " << dist_init << endl;
+        getline(input, line);
+        input >> line >> cycles >> storage_time;
+        //cout << "Cycles " << cycles << " storage_time " << storage_time << endl;
+        getline(input, line);
+        input >> line >> seed_base >> count_step >> frame_time >> check_time;
+        //cout << "seed_base " << seed_base << " count_step " << count_step << " frame_time " << frame_time << " check_time " << check_time << endl;
+        getline(input, line);
+
+        // Initialize system
+        // Initialize particles such that they have distance of dist_init
+        // Do so using just the z-direction
+        // Please don't make dist_init greater than box[2]
+        state[0][2] = -0.5*dist_init;
+        state[1][2] = 0.5*dist_init;
+        Energy(phi);
+        phi_storage = vector<float>(cycles/storage_time,0.0);
+        bond_storage = vector<float>(cycles/storage_time,0.0);
+        state_storage = vector<vector<vector<float>>>(cycles/storage_time, vector<vector<float>>(2, vector<float>(3,0)));
+        generator = Saru(seed_base, count_step);
+    }
+    // also modify config path
+    config_file.open("string_"+to_string(rank_in)+"_config.xyz", std::ios_base::app);
+}
+
+void Dimer::Energy(float& ener_) {
+    // Evaluate energy of system
+    // Evaluate distance
+    float distance = BondLength();
+    float factor_0 = (distance-r_0-width)/width;
+    factor_0 = pow(factor_0,2);
+    factor_0 = 1-factor_0;
+    ener_ = height*pow(factor_0,2);
+}
+
+void Dimer::Energy(float& ener_, float committor_value) {
+    // Evaluate energy of system
+    // Evaluate distance
+    float distance = BondLength();
+    float factor_0 = (distance-r_0-width)/width;
+    factor_0 = pow(factor_0,2);
+    factor_0 = 1-factor_0;
+    ener_ = height*pow(factor_0,2);
+    float factor_com = committor_value - committor_umb;
+    ener_ += 0.5*k_umb*pow(factor_com,2);
+}
+
+void Dimer::BondForce(vector<float>& force_) {
+    // Evaluate bond force
+    // Evaluate distance
+    vector<float> bond_distance(3,0);
+    WrapDistance(state[0],state[1],bond_distance);
+    float distance = pow(bond_distance[0],2);
+    distance += pow(bond_distance[1],2);
+    distance += pow(bond_distance[2],2);
+    float factor_0 = (distance-r_0-width)/width;
+    float factor_1 = pow(factor_0,2);
+    factor_1 = 1-factor_1;
+    float force_scalar = height*factor_0*factor_1/width;
+    force_[0] = bond_distance[0]/distance*force_scalar;
+    force_[1] = bond_distance[1]/distance*force_scalar;
+    force_[2] = bond_distance[2]/distance*force_scalar;
+}
+
+void Dimer::WrapDistance(vector<float>& vec_0, vector<float>& vec_1, vector<float>& vec_return) {
+    vec_return[0] = WrapDistanceSub(vec_0[0], vec_1[0], box[0]);
+    vec_return[1] = WrapDistanceSub(vec_0[1], vec_1[1], box[1]);
+    vec_return[2] = WrapDistanceSub(vec_0[2], vec_1[2], box[2]);
+}
+
+float Dimer::WrapDistanceSub(float& x_0, float& x_1, float& length) {
+    float dx = x_0-x_1;
+    return dx-length*round(dx/length);
+}
+
+void Dimer::PBCWrap(vector<float>& vec) {
+    for(int i=0; i<3; i++) {
+        vec[i] = vec[i]-box[i]*round(vec[i]/box[i]);
+    }
+}
+
+float Dimer::BondLength() {
+    vector<float> bond_distance(3,0);
+    WrapDistance(state[0],state[1],bond_distance);
+    float distance = pow(bond_distance[0],2);
+    distance += pow(bond_distance[1],2);
+    distance += pow(bond_distance[2],2);
+    return pow(distance,0.5);
+}
+
+void Dimer::NormalNumber(float& rand_0, float& rand_1) {
+    // Evaluate random numbers through the Marsaglia polar method
+    // Maybe issue of being exactly 0, but that's a unicorn
+    rand_0 = generator.f(-1,1);
+    rand_1 = generator.f(-1,1);
+    float s = rand_0*rand_0+rand_1*rand_1;
+    while(s>=1) {
+        rand_0 = generator.f(-1,1);
+        rand_1 = generator.f(-1,1);
+        s = rand_0*rand_0+rand_1*rand_1;
+    }
+    float factor = sqrt(-2.0*log(s)/s);
+    rand_0 = rand_0*factor;
+    rand_1 = rand_1*factor;
+}
+
+void Dimer::BDStep() {
+    // Perform Brownian Dynamics step
+    // Initialize random numbers
+    vector<float> random_num(6,0.0);
+    NormalNumber(random_num[0], random_num[1]);
+    NormalNumber(random_num[2], random_num[3]);
+    NormalNumber(random_num[4], random_num[5]);
+    // Rescale by needed factor of sqrt(2*T*dt/mass*gamma)
+    // Consistent with HOOMD's scheme if you track the mass,
+    // and the gamma and dt terms
+    float factor_random = sqrt(2*temp*dt/(mass*gamma));
+    for(int i=0; i<6; i++) {
+        random_num[6] *= factor_random;
+    }
+
+    // Get bond force
+    // Note this is for f_{0} = f_{01}
+    // can get f_{1} = f_{10} through Newton's 3rd law
+    vector<vector<float>> state_old(state);
+    vector<float> force(3,0);
+    BondForce(force);
+    // Now find the updates
+    // Dimer 0
+    for(int i=0; i<3; i++) {
+        state[0][i] += force[i]*dt/(mass*gamma)+random_num[i]; 
+    }
+    // Dimer 1
+    for(int i=0; i<3; i++) {
+        state[1][i] += -force[i]*dt/(mass*gamma)+random_num[3+i]; 
+    }
+    // Now apply PBC correction and done
+    PBCWrap(state[0]);
+    PBCWrap(state[1]);
+}
+
+void Dimer::BiasStep(float* values) {
+    // Add additional term to BD step afterwards
+    // Assume values is 2*3 array
+    for(int i=0; i<2; i++) {
+        for(int j=0; j<3; j++) {
+            state[i][j] += values[j+3*i]*dt/(mass*gamma);
+        }
+    }
+    // Now apply PBC correction and done
+    PBCWrap(state[0]);
+    PBCWrap(state[1]);
+}
+
+void Dimer::Simulate(int steps) {
+    // Run simulation
+    ofstream config_file_2;
+    config_file_2.precision(10);
+    config_file_2.open(config, std::ios_base::app);
+    Energy(phi);
+    for(int i=0; i<steps; i++) {
+        generator = Saru(seed_base, count_step++);
+        BDStep();
+        if(i%check_time==0) {
+            Energy(phi);
+            cout << "Cycle " << i << " phi " << phi << endl;
+        }
+        if(i%storage_time==0) {
+            Energy(phi);
+            float bond_len = BondLength();
+            phi_storage[i/storage_time] = phi;
+            bond_storage[i/storage_time] = bond_len;
+            state_storage[i/storage_time]= state;
+        }
+        if(i%frame_time==0) {
+            DumpXYZ(config_file_2);
+        }
+    }
+
+}
+
+void Dimer::DumpXYZ(ofstream& myfile) {
+    // turns off synchronization of C++ streams
+    ios_base::sync_with_stdio(false);
+    // Turns off flushing of out before in
+    cin.tie(NULL);
+    myfile << 6 << endl;
+    myfile << "# step " << count_step << endl;
+    for(int i=0; i<2; i++) {
+        myfile << "1 " << std::scientific << state[i][0] << " " << std::scientific << state[i][1] << " " << std::scientific << state[i][2] << "\n";
+    }
+}
+
+void Dimer::DumpXYZBias(int val=0) {
+    // turns off synchronization of C++ streams
+    ios_base::sync_with_stdio(false);
+    // Turns off flushing of out before in
+    cin.tie(NULL);
+    config_file << 6 << endl;
+    config_file << "# step " << count_step << " " << committor << " " << phi << " " << phi_umb << " " << committor_umb;
+    config_file << "\n";
+    for(int i=0; i<2; i++) {
+        config_file << "1 " << std::scientific << state[i][0] << " " << std::scientific << state[i][1] << " " << std::scientific << state[i][2] << "\n";
+    }
+}
+
+void Dimer::DumpPhi() {
+    // Evaluate same stats stuff and dump all stored values
+    float phi_ave = 0.0;
+    int storage = cycles/storage_time;
+    for(int i=0; i<storage; i++) {
+        phi_ave += phi_storage[i];
+    }
+    phi_ave /= storage;
+
+    // Standard deviation with Bessel's correction
+    float phi_std = 0.0;
+    for(int i=0; i<storage; i++) {
+        float phi_std_ = phi_ave-phi_storage[i];
+        phi_std += phi_std_*phi_std_;
+    }
+    phi_std = sqrt(phi_std/(storage-1));
+
+    ofstream myfile;
+    myfile.precision(10);
+    myfile.open("phi.txt");
+    myfile << "phi from simulation run" << endl;
+    myfile << "Average " << std::scientific << phi_ave << " Standard_Deviation " << std::scientific << phi_std << endl;
+    myfile.close();
+
+    myfile.open("phi_storage.txt");
+    myfile << "Energy from run" << endl;
+    for(int i=0; i<storage; i++) {
+        myfile << std::scientific << phi_storage[i] << "\n";
+    }
+}
+
+void Dimer::DumpBond() {
+    // Evaluate same stats stuff and dump all stored values
+    float bond_ave = 0.0;
+    int storage = cycles/storage_time;
+    for(int i=0; i<storage; i++) {
+        bond_ave += bond_storage[i];
+    }
+    bond_ave /= storage;
+
+    // Standard deviation with Bessel's correction
+    float bond_std = 0.0;
+    for(int i=0; i<storage; i++) {
+        float bond_std_ = bond_ave-bond_storage[i];
+        bond_std += bond_std_*bond_std_;
+    }
+    bond_std = sqrt(bond_std/(storage-1));
+
+    ofstream myfile;
+    myfile.precision(10);
+    myfile.open("bond.txt");
+    myfile << "bond from simulation run" << endl;
+    myfile << "Average " << std::scientific << bond_ave << " Standard_Deviation " << std::scientific << bond_std << endl;
+    myfile.close();
+
+    myfile.open("bond_storage.txt");
+    myfile << "Energy from run" << endl;
+    for(int i=0; i<storage; i++) {
+        myfile << std::scientific << bond_storage[i] << "\n";
+    }
+}
+
+void Dimer::DumpStates() {
+    int storage = cycles/storage_time;
+    ofstream myfile;
+    myfile.precision(10);
+    myfile.open("state_storage.txt");
+    myfile << "States from run" << endl;
+    for(int i=0; i<storage; i++) {
+        myfile << std::scientific << state_storage[i][0][0] << " " << std::scientific << state_storage[i][0][1] << " " << std::scientific << state_storage[i][0][2] << "\n";
+        myfile << std::scientific << state_storage[i][1][0] << " " << std::scientific << state_storage[i][1][1] << " " << std::scientific << state_storage[i][1][2] << "\n";
+    }
+}
+
+void Dimer::UseRestart() {
+    // Blank function for now
+
+}
+
+int main(int argc, char* argv[]) {
+    Dimer system;
+    system.GetParams("param", 0);
+    system.Simulate(system.cycles);
+    system.DumpPhi();
+    system.DumpBond();
+    system.DumpStates();
+}
---
+++ tpstorch/examples/dimer_ml/dimer.h
@@ -0,0 +1,104 @@
+#include <iostream> 
+#include <fstream>
+#include <random>
+#include <algorithm>
+#include <math.h>
+#include <ctime>
+#include <string>
+#include <vector>
+#include <chrono>
+#include "saruprng.hpp"
+using namespace std;
+#ifndef DIMER_H_
+#define DIMER_H_
+
+class Dimer {
+    public:
+        // Functions
+        // Default constructor
+        Dimer();
+        // Default destructor
+        ~Dimer();
+        // Input parameters
+        void GetParams(string, int);
+        // Input parameters when not using an input file
+        void GetParams();
+        // Get energy
+        void Energy(float&); // normal energy
+        void Energy(float&, float); // energy with committor?
+        // Get force of bond
+        void BondForce(vector<float>&);
+        // Wrap distance when doing operations in PBC
+        void WrapDistance(vector<float>&, vector<float>&, vector<float>&);
+        // WrapDistance subfunction, where third value is length of box in that direction
+        float WrapDistanceSub(float&, float&, float&);
+        // Correct for PBC
+        void PBCWrap(vector<float>&);
+        // Get bond distance
+        float BondLength();
+        // Generate random numbers using Marsaglia polar method
+        void NormalNumber(float&, float&);
+        // Perform Brownian Dynamics step
+        void BDStep();
+        // Add effect of additional force
+        void BiasStep(float*);
+        // Run simulation
+        void Simulate(int);
+        // Run simulation with bias
+        void SimulateBias(int);
+        // Dump state in XYZ format
+        void DumpXYZ(ofstream&);
+        // Dump state in XYZ format with bias
+        void DumpXYZBias(int);
+        // Dump phi_storage 
+        void DumpPhi();
+        // Dump bond_storage 
+        void DumpBond();
+        // Dump state_storage
+        void DumpStates();
+        // Input state
+        void UseRestart();
+        
+        // Variables
+        // Positions
+        vector<vector<float>> state;
+        // Saru RNG
+        Saru generator;
+        unsigned int seed_base = 0;
+        unsigned int count_step = 0;
+        // Dimer system parameters
+        // For the bonded part
+        // V(r) = height*(1-(r-r_0-width)**2/width**2)**2
+        // F(r) = -dV(r)/dr = height*(r-r_0-width)*(1-(r-r_0-width)**2/width**2)/width**2
+        // For the interaction part with (not implemented) solvent, use WCA interactions for everything
+        // but the two particles in a dimer
+        float height = 1.0; // controls the barrier distance
+        float r_0 = 1.0; // controls the contracted distance
+        float width = 1.0; // controls the extended distance
+        // Note extended distance is r_0+2*width
+        // RC is (r-r_0)/2*width
+        // General simulation parameters
+        float box[3] = {1.0,1.0,1.0};
+        float dist_init = 1.0; // initial distance
+        float temp = 1.0; // temperature
+        float gamma = 1.0;
+        float dt = 0.005;
+        float mass = 1.0;
+        float phi = 0.0; // energy
+        float k_umb = 0.0; // harmonic bias strength
+        float phi_umb = 0.0; // energy of umbrella potential
+        float committor = 0.0; // actual committor value
+        float committor_umb = 0.0; // target committor value
+        int cycles = 10000;
+        int storage_time = 1;
+        int frame_time = 10;
+        int check_time = 1000;
+        vector<float> phi_storage;
+        vector<float> bond_storage;
+        vector<vector<vector<float>>> state_storage;
+        string config = "config.xyz";
+        ofstream config_file;
+
+};
+
+#endif
