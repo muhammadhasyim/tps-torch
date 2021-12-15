2dd17b89f4f579b60eae162f180299b1ea4f08cc
Clay H Batton <cbatton@gmail.com>
2021-12-15 14:20:41 -0800

    Having a bad time trying to import adam implementation's, so I stole PyTorch's instead of importing it.

--- tpstorch/ml/optim.py
+++ tpstorch/ml/optim.py
@@ -2,17 +2,57 @@ import torch
 from torch.optim import Optimizer
 from torch.optim.optimizer import required
 
-#Depending on PyTorch version, the name of the functional module
-#May either have an underscore or not!
-oldversion = False
-try:
-    import torch.optim._functional as F
-except:
-    import torch.optim.functional as F
-    oldversion = True
-
 from tpstorch import _rank, _world_size
 from tpstorch import dist
+
+# Stolen adam implementation from PyTorch, weird *, operator kills us everytime
+import math
+from torch import Tensor
+from typing import List, Optional
+def adam(params: List[Tensor],
+         grads: List[Tensor],
+         exp_avgs: List[Tensor],
+         exp_avg_sqs: List[Tensor],
+         max_exp_avg_sqs: List[Tensor],
+         state_steps: List[int],
+         amsgrad: bool,
+         beta1: float,
+         beta2: float,
+         lr: float,
+         weight_decay: float,
+         eps: float):
+    r"""Functional API that performs Adam algorithm computation.
+
+    See :class:`~torch.optim.Adam` for details.
+    """
+
+    for i, param in enumerate(params):
+
+        grad = grads[i]
+        exp_avg = exp_avgs[i]
+        exp_avg_sq = exp_avg_sqs[i]
+        step = state_steps[i]
+
+        bias_correction1 = 1 - beta1 ** step
+        bias_correction2 = 1 - beta2 ** step
+
+        if weight_decay != 0:
+            grad = grad.add(param, alpha=weight_decay)
+
+        # Decay the first and second moment running average coefficient
+        exp_avg.mul_(beta1).add_(grad, alpha=1 - beta1)
+        exp_avg_sq.mul_(beta2).addcmul_(grad, grad.conj(), value=1 - beta2)
+        if amsgrad:
+            # Maintains the maximum of all 2nd moment running avg. till now
+            torch.maximum(max_exp_avg_sqs[i], exp_avg_sq, out=max_exp_avg_sqs[i])
+            # Use the max. for normalizing running avg. of gradient
+            denom = (max_exp_avg_sqs[i].sqrt() / math.sqrt(bias_correction2)).add_(eps)
+        else:
+            denom = (exp_avg_sq.sqrt() / math.sqrt(bias_correction2)).add_(eps)
+
+        step_size = lr / bias_correction1
+
+        param.addcdiv_(exp_avg, denom, value=-step_size)
 
 class ParallelAdam(Optimizer):
     r"""Implements Adam algorithm.
@@ -103,7 +143,7 @@ class ParallelAdam(Optimizer):
                     # record the step after step update
                     state_steps.append(state['step'])
 
-            F.adam(params_with_grad,
+            adam(params_with_grad,
                    grads,
                    exp_avgs,
                    exp_avg_sqs,
