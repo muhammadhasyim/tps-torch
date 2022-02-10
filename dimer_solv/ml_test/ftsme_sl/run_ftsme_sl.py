17a5653d400d981f58f259891dde41da443fc7e8
Clay Batton <cbatton35@gmail.com>
2022-02-09 16:04:14 -0800

    Renamed things to be more compliant

--- dimer_solv/ml_test/ftsme_sl/run_ftsme_sl.py
+++ dimer_solv/ml_test/ftsme_sl/run_ftsme_sl.py
@@ -1,5 +1,7 @@
 import sys
 sys.path.append("..")
+import time
+t0 = time.time()
 
 #Import necessarry tools from torch
 import tpstorch
@@ -10,7 +12,7 @@ import scipy.spatial
 
 #Import necessarry tools from tpstorch 
 from dimer_fts_nosolv import dimer_reorient, DimerFTS
-from committor_nn import initializeConfig, CommittorNet, CommittorNetBP, CommittorNetDR
+from committor_nn import initializeConfig, CommittorNet, CommittorNetBP, CommittorNetDR, SchNet
 from dimer_fts_nosolv import FTSLayerCustom as FTSLayer
 from tpstorch.ml.data import FTSSimulation
 from tpstorch.ml.optim import ParallelSGD, ParallelAdam, FTSUpdate
@@ -31,28 +33,34 @@ prefix = 'simple'
 
 #(1) Initialization
 r0 = 2**(1/6.0)
-width =  0.5*r0
+width =  0.25
 Np = 30+2
-box = [14.736125994561544, 14.736125994561544, 14.736125994561544]
+box = [8.617738760127533, 8.617738760127533, 8.617738760127533]
 kT = 1.0
 
-start, end, initial_config = initializeConfig(rank/(world_size-1), r0, width, box,Np)
+initial_config = np.genfromtxt("../restart/config_"+str(rank)+".xyz", usecols=(1,2,3))
+start = np.genfromtxt("../restart_bc/config_"+str(rank)+"_react.xyz", usecols=(1,2,3))
+end = np.genfromtxt("../restart_bc/config_"+str(rank)+"_prod.xyz", usecols=(1,2,3))
+initial_config = torch.from_numpy(initial_config)
+start = torch.from_numpy(start)
+end = torch.from_numpy(end)
+initial_config = initial_config.float()
+start = start.float()
+end = end.float()
 
 #Initialize neural net
-#committor = CommittorNetDR(num_nodes=2500, boxsize=10).to('cpu')
-committor = torch.jit.script(CommittorNetBP(num_nodes=200, boxsize=box[0], Np=32,rc=2.5,sigma=1.0).to('cpu'))
+#committor = torch.jit.script(CommittorNetDR(num_nodes=2500, boxsize=box[0]).to('cpu'))
+#committor = torch.jit.script(CommittorNetBP(num_nodes=200, boxsize=box[0], Np=32,rc=2.5,sigma=1.0).to('cpu'))
+committor = SchNet(hidden_channels = 64, num_filters = 64, num_interactions = 3, num_gaussians = 50, cutoff = box[0], max_num_neighbors = 31, boxsize=box[0], Np=32, dim=3).to('cpu')
 
 #Initialize the string for FTS method
 ftslayer = FTSLayer(react_config=start[:2].flatten(),prod_config=end[:2].flatten(),num_nodes=world_size,boxsize=box[0],num_particles=2).to('cpu')
-
-#Load the pre-initialized neural network and string
-committor.load_state_dict(torch.load("../initial_1hl_nn_bp"))
+committor.load_state_dict(torch.load("initial_1hl_nn", map_location=torch.device('cpu')))
 ftslayer.load_state_dict(torch.load("../test_string_config"))
 
 n_boundary_samples = 100
 batch_size = 8
 period = 25
-#Initialize the dimer simulation
 #Initialize the dimer simulation
 dimer_sim_bc = DimerFTS(param="param_bc",
                         config=initial_config.detach().clone(), 
@@ -86,9 +94,9 @@ dimer_sim_com = DimerFTS(param="param",
 datarunner = FTSSimulation(dimer_sim, committor = committor, nn_training = True, period=period, batch_size=batch_size, dimN=Np*3)
 #Construct optimizers
 ftsoptimizer = FTSUpdate(ftslayer.parameters(), deltatau=0.01,momentum=0.9,nesterov=True,kappa=0.1,periodic=True,dim=3)
-optimizer = ParallelAdam(committor.parameters(), lr=3e-3)
+optimizer = ParallelAdam(committor.parameters(), lr=1e-4)
 
-#Initialize loss functions
+#Initialize main loss function
 loss = BKELossFTS(  bc_sampler = dimer_sim_bc,
                     committor = committor,
                     lambda_A = 1e4,
@@ -96,7 +104,7 @@ loss = BKELossFTS(  bc_sampler = dimer_sim_bc,
                     start_react = start,
                     start_prod = end,
                     n_bc_samples = 100, 
-                    bc_period = 10,
+                    bc_period = 100,
                     batch_size_bc = 0.5,
                     tol = 5e-10,
                     mode= 'shift')
@@ -105,14 +113,15 @@ cmloss = CommittorLoss2( cl_sampler = dimer_sim_com,
                         committor = committor,
                         lambda_cl=100.0,
                         cl_start=10,
-                        cl_end=200,
+                        cl_end=5000,
                         cl_rate=10,
-                        cl_trials=50,
+                        cl_trials=100,
                         batch_size_cl=0.5
                         )
 
 loss_io = []
-loss_io = open("{}_statistic_{}.txt".format(prefix,rank+1),'w')
+if rank == 0:
+    loss_io = open("{}_statistic_{}.txt".format(prefix,rank+1),'w')
 
 #Training loop
 lambda_cl_end = 10**3
@@ -120,18 +129,28 @@ cl_start=200
 cl_end=10000
 cl_stepsize = (lambda_cl_end-cmloss.lambda_cl)/(cl_end-cl_start)
 
+# Save reactant, product configurations
+torch.save(loss.react_configs, "react_configs_"+str(rank+1)+".pt")
+torch.save(loss.prod_configs, "prod_configs_"+str(rank+1)+".pt")
+torch.save(loss.n_bc_samples, "n_bc_samples_"+str(rank+1)+".pt")
+
 #We can train in terms of epochs, but we will keep it in one epoch
 with open("string_{}_config.xyz".format(rank),"w") as f, open("string_{}_log.txt".format(rank),"w") as g:
     for epoch in range(1):
         if rank == 0:
             print("epoch: [{}]".format(epoch+1))
-        for i in tqdm.tqdm(range(10000)):#20000)):
+        time_max = 9.0*60
+        time_out = True
+        #for i in range(count,count+100):#20000)):
+        i = 0
+        while(time_out):
             if (i > cl_start) and (i <= cl_end):
                 cmloss.lambda_cl += cl_stepsize
             elif i > cl_end:
                 cmloss.lambda_cl = lambda_cl_end
             # get data and reweighting factors
             configs, grad_xs  = datarunner.runSimulation()
+            dimer_sim.dumpRestart()
             
             # zero the parameter gradients
             optimizer.zero_grad()
@@ -145,7 +164,7 @@ with open("string_{}_config.xyz".format(rank),"w") as f, open("string_{}_log.txt
             
             optimizer.step()
             
-            ftsoptimizer.step(configs[:,:6],len(configs),boxsize=box[0],reorient_sample=dimer_reorient)
+            #ftsoptimizer.step(configs[:,:6],len(configs),boxsize=box[0],reorient_sample=dimer_reorient)
 
             # print statistics
             with torch.no_grad():
@@ -154,17 +173,36 @@ with open("string_{}_config.xyz".format(rank),"w") as f, open("string_{}_log.txt
                 f.write('Lattice=\"10.0 0.0 0.0 0.0 10.0 0.0 0.0 0.0 10.0\" ')
                 f.write('Origin=\"-5.0 -5.0 -5.0\" ')
                 f.write("Properties=type:S:1:pos:R:3:aux1:R:1 \n")
-                f.write("2 {} {} {} {} \n".format(string_temp[0,0],string_temp[0,1], string_temp[0,2],0.5*r0))
-                f.write("2 {} {} {} {} \n".format(string_temp[1,0],string_temp[1,1], string_temp[1,2],0.5*r0))
+                f.write("2 {} {} {} {} \n".format(string_temp[0,0],string_temp[0,1], string_temp[0,2],0.25))
+                f.write("2 {} {} {} {} \n".format(string_temp[1,0],string_temp[1,1], string_temp[1,2],0.25))
                 f.flush()
                 g.write("{} {} \n".format((i+1)*period,torch.norm(string_temp[0]-string_temp[1])))
                 g.flush()
-                #if counter % 10 == 0:
+                
                 main_loss = loss.main_loss
                 bc_loss = loss.bc_loss
+                cm_loss = cmloss.cl_loss
                 
-                loss_io.write('{:d} {:.5E} {:.5E} \n'.format(i+1,main_loss.item(),bc_loss.item()))
-                loss_io.flush()
                 #Print statistics 
                 if rank == 0:
-                    torch.save(committor.state_dict(), "{}_params_t_{}_{}".format(prefix,i,rank))
+                    if i%100 == 0:
+                        torch.save(committor.state_dict(), "{}_params_t_{}_{}".format(prefix,i,rank))
+                        torch.save(ftslayer.state_dict(), "{}_string_t_{}_{}".format(prefix,i,rank))
+                    torch.save(committor.state_dict(), "{}_params".format(prefix))
+                    torch.save(ftslayer.state_dict(), "{}_string".format(prefix))
+                    torch.save(optimizer.state_dict(), "optimizer_params")
+                    torch.save(ftsoptimizer.state_dict(), "ftsoptimizer_params")
+                    np.savetxt("count.txt", np.array((i+1,)))
+                    loss_io.write('{:d} {:.5E} {:.5E} {:.5E}\n'.format(i+1,main_loss.item(),bc_loss.item(),cm_loss.item()))
+                    loss_io.flush()
+                torch.save(cmloss.lambda_cl, "lambda_cl_"+str(rank+1)+".pt")
+                torch.save(cmloss.cl_configs, "cl_configs_"+str(rank+1)+".pt")
+                torch.save(cmloss.cl_configs_values, "cl_configs_values_"+str(rank+1)+".pt")
+                torch.save(cmloss.cl_configs_count, "cl_configs_count_"+str(rank+1)+".pt")
+                i = i+1
+                t1 = time.time()
+                time_diff = t1-t0
+                time_diff = torch.tensor(time_diff)
+                dist.all_reduce(time_diff,op=dist.ReduceOp.MAX)
+                if time_diff > time_max:
+                    time_out = False
